bfee3cf4f4157ffad1c1f6b3f773077678bdcdb2
amine123hub <mohamedgithub2000@mail.com>
2025-10-02 15:05:13 +0100

    added digitalbank landing page files project

---
+++ script.js
@@ -0,0 +1,8 @@
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.nav-links');
+
+navbarToggle.addEventListener('click', () => {
+navbarToggle.classList.toggle('active');
+navbarMenu.classList.toggle('active');
+
+})
